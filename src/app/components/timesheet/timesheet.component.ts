d26fac890d3764cd0a83a8086e52ac6619234f4d
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-03-11 09:15:14 -0700

    Added validator to check for duplicate names

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Department } from 'src/app/interfaces/department';
 import { DepartmentsService } from 'src/app/services/departments.service';
-import { FormControl } from "@angular/forms";
 import { Employee } from "../../interfaces/employee";
+import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-timesheet',
@@ -13,7 +15,7 @@ import { Employee } from "../../interfaces/employee";
 export class TimesheetComponent implements OnInit {
   departments: Department[];
   department: Department;
-  employeeNameFC = new FormControl('');
+  employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
   employeeId = 0;
 
@@ -40,6 +42,20 @@ export class TimesheetComponent implements OnInit {
 
       this.employeeNameFC.setValue('');
     }
+  }
+
+  nameValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      let error = null;
+      if (this.employees && this.employees.length) {
+        this.employees.forEach(employee => {
+          if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+            error = { duplicate: true };
+          }
+        });
+      }
+      return error;
+    };
   }
 
 }
