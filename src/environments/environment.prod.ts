3f73407b8989b49265bb06c46de1209109e90f3c
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-06-22 17:21:36 -0600

    Build app for gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCB038zknTcMwiTfmO1bycz1gKOz7Wj0AM",
+    authDomain: "hr-timesheet-91a6f.firebaseapp.com",
+    projectId: "hr-timesheet-91a6f",
+    storageBucket: "hr-timesheet-91a6f.appspot.com",
+    messagingSenderId: "786718473338",
+    appId: "1:786718473338:web:f17e1f6f9da12a52f64fc3"
+  }
 };
